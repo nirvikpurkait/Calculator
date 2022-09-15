8dcf61f9428c0cb92209ca4379c8f4feda4ffc4c
NirvikPurkait <93871471+NirvikPurkait@users.noreply.github.com>
2022-09-16 05:27:43 +0530

    made some changes

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
+const allButton = document.querySelector('.all-button');
 const button = Array.from(document.querySelectorAll('button'));
-
+const screen = document.querySelector('.screen');
 let message = '';
+
+
+screen.style.width = `${allButton.clientWidth}px`
+
 
 button.forEach(element => {
   element.addEventListener('click', () => {
 
     if (element.classList[1] == 'number') {
       let ultimateNumber = Number(message += element.innerHTML)
-      console.log(ultimateNumber);
-      console.log("that is a number");
+      if (isNaN(ultimateNumber)) {
+        screen.innerHTML = `Please provide a valid number`
+        console.log(`please provide a valid number`);
+        return
+      }
+      else {
+        console.log(ultimateNumber);
+        screen.innerHTML = message
+        console.log("that is a number");
+      }
     }
 
     else if (element.classList[1] == 'operator') {
@@ -25,3 +38,8 @@ button.forEach(element => {
     }
   })
 })
+
+
+
+
+
