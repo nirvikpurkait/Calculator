e938d97c6017460ab91b10ca439573e791a50611
NirvikPurkait <93871471+NirvikPurkait@users.noreply.github.com>
2022-09-17 05:26:11 +0530

    added SSD and improved some feature

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
+const screen = document.querySelector('.screen');
+const screenMessage = document.querySelector('.message');
+const screenResult = document.querySelector('.result');
 const allButton = document.querySelector('.all-button');
 const button = Array.from(document.querySelectorAll('button'));
-const screen = document.querySelector('.screen');
-let message = '';
-
 
 screen.style.width = `${allButton.clientWidth}px`
 
+let text = ''
 
-button.forEach(element => {
+button.forEach((element) => {
   element.addEventListener('click', () => {
 
-    if (element.classList[1] == 'number') {
-      let ultimateNumber = Number(message += element.innerHTML)
-      if (isNaN(ultimateNumber)) {
-        screen.innerHTML = `Please provide a valid number`
-        console.log(`please provide a valid number`);
+    if (element.classList[0] == 'point') {
+      let dot = document.querySelector('.point').innerHTML;
+      if (text.includes(dot)) {
+        screenMessage.innerHTML = `Can't be more than one decimal point in a number`
         return
       }
       else {
-        console.log(ultimateNumber);
-        screen.innerHTML = message
-        console.log("that is a number");
+        text += dot
       }
+      console.log(text);
+      screenResult.innerHTML = text
     }
-
+    else if (element.classList[1] == 'number') {
+      text += element.innerHTML        
+      console.log(text);
+      screenResult.innerHTML = text
+    }
     else if (element.classList[1] == 'operator') {
-      message = ''
-      console.log("that is a operator");
     }
-      
-    else if(element.classList[0] == 'all-clear') {
-      console.log("that is a all clear button");
+    else if (element.classList[0] == 'all-clear') {
+      text = ''
+      screenResult.innerHTML = text
     }
-      
-    else if(element.classList[0] == 'back-space') {
-      console.log("that is a back space button");
+    else if (element.classList[0] == 'delete') {
+      text = text.slice(0, -1)
+      console.log(text);
+      screenResult.innerHTML = text
     }
   })
 })
 
 
-
-
-
+// console.log(Number(''));
